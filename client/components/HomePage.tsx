e2e50be753136793d93d0f5d6475621bef18189a
anthony-santiago <129936071+anthony2515@users.noreply.github.com>
2023-11-10 09:55:31 +1300

    displays raw weather data

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -1,39 +1,28 @@
 import { useState, useEffect } from 'react'
-import { getGreeting } from '../apiClient.ts'
+import { getWeather } from '../apiClient.ts'
 import { Link } from 'react-router-dom'
 
 function HomePage() {
-  const [greeting, setGreeting] = useState('')
-  const [count, setCount] = useState(0)
-  const [isError, setIsError] = useState(false)
+  const [weather, setWeather] = useState('no data')
+ 
 
   useEffect(() => {
-    async function updateGreeting() {
+    async function updateWeather() {
       try {
-        const greeting = await getGreeting()
-        setGreeting(greeting)
-        setIsError(false)
+        const newWeatherData = await getWeather()
+        setWeather(newWeatherData)
+       
       } catch (err) {
-        setIsError(true)
+       console.error(err.message)
       }
     }
 
-    updateGreeting()
-  }, [count])
+    updateWeather()
+  }, [])
 
   return (
     <>
-      {count}
-      <h1>{greeting}</h1>
-      {isError && (
-        <p style={{ color: 'red' }}>
-          There was an error retrieving the greeting.
-        </p>
-      )}
-      <button onClick={() => setCount(count + 1)}>Click</button>
-      <p>
-        <Link to="/frogs/kevin">Is there a frog named Kevin?</Link>
-      </p>
+      {weather}
     </>
   )
 }
